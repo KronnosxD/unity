49672a8032ced9954eedf652393a7770c9458f89
Nicolas <asdpof@gmail.com>
2018-05-16 20:43:09 -0400

    ...

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict'
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-var user_routes = require('./routes/user');
+var player_routes = require('./routes/player.route');
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -14,5 +19,5 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api', user_routes);
+app.use('/api', player_routes);
 module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict'
 var app = require('./app');
-var PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-        console.log('App listening on ', PORT);
-        console.log('Press Ctrl+C to quit.');
-});
+
+var mongoose = require('mongoose');
+
+mongoose.Promise  = global.Promise;
+mongoose.connect('mongodb://K4l44dmin:K4L4B4S3@ds123500.mlab.com:23500/kala')
+
+.then(() => {
+        console.log("Conexión con MongoDB establecida");
+
+        var port = process.env.port || 8080;
+        app.listen(port, () => {
+                console.log("Servidor Node está corriendo en el puerto: " + port)
+        })
+})
+.catch(err => console.log(err));
