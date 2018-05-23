d836e6dd08650885e3f3b5b9b2cbc1ad2034a491
naguerovalencia <nicolas.aguero04@inacapmail.cl>
2018-05-23 18:23:55 -0400

    ya tu sae
    asasd

--- scripts unity/playerController.cs
+++ scripts unity/playerController.cs
@@ -1,20 +1,51 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
-    public List<string> objetos;
-	// Use this for initialization
+    public JArray objetos;
+
 	void Start () {
-        objetos = new List<string>();
+        
+        objetos = new JArray();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
+        createInventory();
+    }
+    void createInventory()
+    {
+      
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            JObject inventoryJSON = new JObject(
+                new JProperty("inventory",
+                    new JArray(
+                            from items in objetos
+                            select new JObject(
+                                    new JProperty("itemId", items)
+                                )
+                        )
+                )
+            );
+            string path = @"c:\temp\MyTest.txt";
+            if (!File.Exists(path))
+            {
+              
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(inventoryJSON.ToString());
+                }
+            }
+
+         
+        }
         
     }
-
 }
 
 
